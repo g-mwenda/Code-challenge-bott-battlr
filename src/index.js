6739f3ed2d96b98f25e2b6331fe8f850bf820bbf
Gabriel <gabrielmwenda0001@gmail.com>
2023-05-01 01:50:59 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {createRoot} from "react-dom/client"
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+
+import "./styles/index.css"
+
+
+let nodeRoot = document.getElementById("root");
+let root = createRoot(nodeRoot);
+root.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>
+)
